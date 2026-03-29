07d22158b023cb349718d84fffd8ca1c66b3f855
Sri Manikanta <srimanikantayedida18@gmail.com>
2026-03-29 18:03:11 +0530

    First commit of player

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import songs from "./songs";
 
-function App() {
+function App(){
+  const [currentSong ,setCurrentSong] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+     <div>
+      <h1>Music Player</h1>
+
+      {songs.map((song,index) => (
+        <button key = {index} onClick={() => setCurrentSong(song)}>{song.title}</button>
+      ))}
+
+      {currentSong && (
+        <audio controls autoPlay src = {currentSong.file}></audio>
+      )}
+     </div>
+  )
+}
 export default App;
+
---
+++ src/songs.js
@@ -0,0 +1,12 @@
+const songs = [
+
+  {
+    title: "Song 1",
+    file: "/songs/song1.mp3",
+  },
+  {
+    title:"Song 2",
+    file: "/songs/song2.mp3",
+  },
+];
+export default songs;
