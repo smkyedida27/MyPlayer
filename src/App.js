48978bd2d87981092aa5bc24660ada0699851039
Sri Manikanta <srimanikantayedida18@gmail.com>
2026-04-02 23:29:07 +0530

    S

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React,{useState} from "react";
-import songs from "./songs";
+  import React,{useState} from "react";
+  import songs from "./songs";
 
-function App(){
-  const [currentSong ,setCurrentSong] = useState(null);
+  function App(){
+    const [currentIndex ,setCurrentIndex] = useState(0);
 
-  return (
+    const nextSong = () => {
+      setCurrentIndex((prev) => (prev + 1) % songs.length);
+    };
+    const prevSong = () => {
+        setCurrentIndex((prev) => 
+        prev === 0 ? songs.length - 1 : prev - 1);
+    };
+    return (
+      <div>
+        <h1>Music Player</h1>
 
-     <div>
-      <h1>Music Player</h1>
-
-      {songs.map((song,index) => (
-        <button key = {index} onClick={() => setCurrentSong(song)}>{song.title}</button>
-      ))}
-
-      {currentSong && (
-        <audio controls autoPlay src = {currentSong.file}></audio>
-      )}
-     </div>
-  )
-}
-export default App;
+        {songs.map((song,index) => (
+          <button key = {index} onClick={() => setCurrentIndex(index)}>{song.title}</button>
+        ))}
+        <audio key={songs[currentIndex].file} controls autoPlay src={songs[currentIndex].file}></audio>
+        <div>
+          <button onClick={prevSong}>⬅️Prev</button>
+          <button onClick={nextSong}>Next➡️ </button>
+        </div>
+        
+      </div>
+    );
+  }
+  export default App;
+//COMMIT
 
